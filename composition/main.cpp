a70c5c2128654ebd73134c6ba348aec6a1d57985
NickBwalley <nickbwalley@gmail.com>
2020-05-17 05:25:23 +0300

    cpp_programs

--- composition/main.cpp
+++ composition/main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 int main()
 //concept objects of another class as their members
 //using objects of other classes within your class
+//note that you must include the header files otherwise the compiler will complain 
 {
     birthday birthObj(8,26,2000);
     people nickie("NickBwalley", birthObj);
--- function_const/main.cpp
+++ function_const/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "nicky.h"
-
+//adding the nicky.h header file for the file to be recognised before running the program
 using namespace std;
 
 int main()
