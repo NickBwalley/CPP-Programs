8dc6a18abb8a179c25a66a151da97575d6faddb2
NickBwalley <nickbwalley@gmail.com>
2020-05-29 07:15:14 +0300

    cpp

--- Pointer_Programs/main.cpp
+++ Pointer_Programs/main.cpp
@@ -3,9 +3,11 @@
 using namespace std;
 //function prototype
 void addNumber();
+void multiplyNumbers();
 int main()
 {
-    addNumber();
+    //addNumber();
+    multiplyNumbers();
 }
 
 //function add Numbers using pointers
@@ -21,5 +23,23 @@ void addNumber(){
  z = &res;
  *z = *x+*y;
  cout << "Result is: " << *z << endl;
+
+}
+
+//function to multiply number using pointers
+void multiplyNumbers(){
+    int p, q, res;
+    cout << "Enter firstNumber: " << endl;
+    cin >> p;
+    cout << "Enter secondNumber: " << endl;
+    cin >> q;
+
+    int *a, *b, *c;
+    a = &p;
+    b = &q;
+    c = &res;
+
+    *c = *a * *b;
+    cout << "Result is: " << *c << endl;
 
 }
