f8e85d88850898bb5b1977b56af73c3177cd5fd4
NickBwalley <nickbwalley@gmail.com>
2020-06-02 01:58:34 +0300

    cpp

--- Concepts_Practice/main.cpp
+++ Concepts_Practice/main.cpp
@@ -1,9 +1,20 @@
-#include <iostream>
+#include <iostream> //#include-> adding header files >> <iostream> means you include input output streams like cout cin
 
 using namespace std;
+//creating function prototype
+void printSomething(int a, int b);
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    printSomething(499,99);
     return 0;
 }
+
+//function printing certain if statements
+void printSomething(int a, int b){
+    if(a > b){
+        cout << a << " is the greatest Number..." << endl;
+    }else{
+        cout << b << " is the greatest Number..." << endl;
+    }
+ }
