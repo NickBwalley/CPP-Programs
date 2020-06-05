f04881345a843027d961a2412a4e4a3af48a1658
NickBwalley <nickbwalley@gmail.com>
2020-06-06 02:41:41 +0300

    cpp

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -13,8 +13,10 @@ void printArray(){
     int arraySize, arrayElements;
     cout << "Enter the Size of the Array: " << endl;
     cin >> arraySize;
+    int nicky[arraySize];
     cout << "Enter the Elements of Your Array: " << endl;
-    for(int i = 0; i < arraySize; i++){
-        cin >> arrayElements;
+    for(int i = 0; i < nicky[arraySize]; i++){
+        cin >> nicky[i];
     }
+    cout << nicky[2] << endl;
 }
