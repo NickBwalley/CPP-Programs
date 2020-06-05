4c7745bbcccdab825b1c3a75126481ef9792f969
NickBwalley <nickbwalley@gmail.com>
2020-06-06 02:33:29 +0300

    cpp

--- Functions_sizeOfFunction/main.cpp
+++ Functions_sizeOfFunction/main.cpp
@@ -2,10 +2,15 @@
 
 using namespace std;
 
+void sizeOfArray();
 int main()
 {
     //main concept is to figure out how to calculate the size of a certain variable stored
-    double nicky[10];
-    cout << "size of this array: " << sizeof(nicky) << endl;
-    cout << "Number of Elements is Array: " << sizeof(nicky) / sizeof(nicky[0]) <<endl;
+    sizeOfArray();
+}
+
+void sizeOfArray(){
+    int rikki[5] = {12,13,14,15,16};
+    cout << "Size of Array: " << sizeof(rikki) << endl;
+    cout << "Number of Elements: " << sizeof(rikki) / sizeof(rikki[0]);
 }
