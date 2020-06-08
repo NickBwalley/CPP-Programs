a4fff1316e9b249942a5f3a1fa9ab468617e94c2
NickBwalley <nickbwalley@gmail.com>
2020-06-08 03:15:21 +0300

    cpp

--- Classes_SentinelProgram/main.cpp
+++ Classes_SentinelProgram/main.cpp
@@ -2,26 +2,8 @@
 
 using namespace std;
 
-class nicky{
-private:
-    int number;
-    int total = 0;
-public:
-    void sentinelProgram(){
-        cout << "**SENTINEL CONTROLLED PROGRAM!..**" << endl;
-        cout << "Enter Your Numbers(Press -1 to QUIT): " << endl;
-        cin >> number;
-        while(number != -1){
-            cout << "Enter Another Number: (Press -1 QUIT and Get Total): " << endl;
-            cin >> number;
-            total+=number;
-        }
-        cout << "Total: " << total << endl;
-    }
-};
-
 int main()
 {
-    nicky bwalley;
-    bwalley.sentinelProgram();
+    cout << "Hello world!" << endl;
+    return 0;
 }
