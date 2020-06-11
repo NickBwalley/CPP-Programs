ea4a95cb2e44a9a8d08b4b3e86da98a242803b91
NickBwalley <nickbwalley@gmail.com>
2020-06-12 00:29:31 +0300

    cpp

---
+++ BubbleSort/NickFury.h
@@ -0,0 +1,16 @@
+#ifndef NICKFURY_H
+#define NICKFURY_H
+
+
+class NickFury
+{
+    public:
+        NickFury();
+
+
+    protected:
+
+    private:
+};
+
+#endif // NICKFURY_H
--- BubbleSort/main.cpp
+++ BubbleSort/main.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 using namespace std;
+#include "NickFury.h"
 void swapping(int &a, int &b) {      //swap the content of a and b
    int temp;
    temp = a;
@@ -25,17 +26,19 @@ void bubbleSort(int *array, int size) {
    }
 }
 int main() {
-   int n;
-   cout << "Enter the number of elements: ";
-   cin >> n;
-   int arr[n];     //create an array with given number of elements
-   cout << "Enter elements:" << endl;
-   for(int i = 0; i<n; i++) {
-      cin >> arr[i];
-   }
-   cout << "Array before Sorting: ";
-   display(arr, n);
-   bubbleSort(arr, n);
-   cout << "Array after Sorting: ";
-   display(arr, n);
+//   int n;
+//   cout << "Enter the number of elements: ";
+//   cin >> n;
+//   int arr[n];     //create an array with given number of elements
+//   cout << "Enter elements:" << endl;
+//   for(int i = 0; i<n; i++) {
+//      cin >> arr[i];
+//   }
+//   cout << "Array before Sorting: ";
+//   display(arr, n);
+//   bubbleSort(arr, n);
+//   cout << "Array after Sorting: ";
+//   display(arr, n);
+/****************************************************************/
+
 }
