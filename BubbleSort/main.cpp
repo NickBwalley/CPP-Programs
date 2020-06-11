19ebc4673fd8f4fdd90ec57ea943ffa6265b9d35
NickBwalley <nickbwalley@gmail.com>
2020-06-11 04:44:29 +0300

    cpp

--- BubbleSort/main.cpp
+++ BubbleSort/main.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
 
 using namespace std;
+//function prototype
+void display()
 
 int main()
 {
-    int sampleArray[8] = {91,49,41,77,84,93,66,57};
+    int n;
+    cout << "Declare the Size of Your Array: " << endl;
+    cin >> n;
+    int nick[n];
+    cout << "Enter the Elements of Your Array: " << endl;
+    for(int i = 0; i < n; i++){
+        cin >> nick[i];
+    }
+    display(nick, n);
+    bubbleSort(nick, n);
 
 }
-
+//function to print out the elements inside the user defined array
+void display(*Array, n){
+    for(int z = 0; z < n; z++)
+        cout << Array[z] << " ";
+    cout << endl;
+}
