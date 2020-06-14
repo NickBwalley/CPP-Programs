da49c4430cb1a020a6f7a833f513d11bc012c9be
NickBwalley <nickbwalley@gmail.com>
2020-06-14 07:50:05 +0300

    cpp

--- PrimeNumber_Checker/main.cpp
+++ PrimeNumber_Checker/main.cpp
@@ -1,32 +1,35 @@
 #include <iostream>
-
 using namespace std;
+//function prototype
+void primeNumberChecker();
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    void primeNumberChecker();
+
 }
 
+
 void primeNumberChecker(){
-    int n;
-    int m = 0;
-    bool flag = false;
-
-    cout << "Enter A Positive Number: " << endl;
-    cin >> n;
-    m = n/2;
-    for(int i = 2; i <= m; i++){
-        if( n % i == 0){
-            flag = true;
-            break;
-        }
-    }
-
-    if(flag == false){
-        cout << n << " is a Prime Number.." << endl;
-
-    }
-    else
-        cout << n << " is Not a Prime Number..." << endl;
+    cout << "This is the primeNumberChecker" << endl;
+//    int n;
+//    int m = 0;
+//    bool flag = false;
+//
+//    cout << "Enter A Positive Number: " << endl;
+//    cin >> n;
+//    m = n/2;
+//    for(int i = 2; i <= m; i++){
+//        if( n % i == 0){
+//            flag = true;
+//            break;
+//        }
+//    }
+//
+//    if(flag == false){
+//        cout << n << " is a Prime Number.." << endl;
+//
+//    }
+//    else
+//        cout << n << " is Not a Prime Number..." << endl;
 }
