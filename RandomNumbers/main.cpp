a6adacce9b93ce009b405ca47f007bfcf1acd524
NickBwalley <nickbwalley@gmail.com>
2020-06-16 03:43:10 +0300

    cpp

--- RandomNumbers/main.cpp
+++ RandomNumbers/main.cpp
@@ -3,9 +3,12 @@
 using namespace std;
 
 int main(){
+    cout << "Random Number 1" << endl;
+    cout << "------------------" << endl;
     randomNumber nicky;
     nicky.printRandom();
     cout << "------------------" << endl;
+    cout << "Random Number 2" << endl;
     nicky.printRandom2();
     return 0;
 }
