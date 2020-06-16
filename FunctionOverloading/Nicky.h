af4cdbf17448bba289ac4aa9055b1921eb4a7d33
NickBwalley <nickbwalley@gmail.com>
2020-06-17 00:41:15 +0300

    cpp

---
+++ FunctionOverloading/Nicky.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Nicky.h"
+using namespace std;
+
+class Nicky
+{
+    public:
+        void overloaded(float x);
+        void overloaded(int x);
+
+    protected:
+
+    private:
+};
+
+
