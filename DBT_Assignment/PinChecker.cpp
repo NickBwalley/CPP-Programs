1309ae2e8301f098c58d2333b5ca3d82315a7ddf
NickBwalley <nickbwalley@gmail.com>
2020-07-06 11:00:49 +0300

    DBT1101

--- DBT_Assignment/PinChecker.cpp
+++ DBT_Assignment/PinChecker.cpp
@@ -5,6 +5,20 @@ using namespace std;
 PinChecker::PinChecker()
 {
     const int PIN = 1234;
-    cout << PIN << endl;
+    int pin;
+    cout << "Enter 4-Digit PIN-NUMBER: " << endl;
+    cin >> pin;
+    if(pin != PIN)
+        cout << "Incorrect PIN!.." << endl;
+        cout << "-------------------------------" << endl;
+        cout << "You have 3Trials Remaining..." << endl;
+        cout << "-------------------------------" << endl;
+
+    for(int trials = 0; trials < 3; trials++){
+        cout << "Enter a 4-Digit PIN-NUMBER: " << endl;
+        cin >> pin;
+    }
+
+
 
 }
