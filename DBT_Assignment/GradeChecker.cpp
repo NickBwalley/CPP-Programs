fa62e29b8dbd41a64942b6f48f7fe00583d428c7
NickBwalley <nickbwalley@gmail.com>
2020-07-06 11:35:55 +0300

    DBT1101

---
+++ DBT_Assignment/GradeChecker.cpp
@@ -0,0 +1,6 @@
+#include "GradeChecker.h"
+
+GradeChecker::GradeChecker()
+{
+    //ctor
+}
---
+++ DBT_Assignment/GradeChecker.h
@@ -0,0 +1,15 @@
+#ifndef GRADECHECKER_H
+#define GRADECHECKER_H
+
+
+class GradeChecker
+{
+    public:
+        GradeChecker();
+
+    protected:
+
+    private:
+};
+
+#endif // GRADECHECKER_H
