14689c8eede68cd668985bbcac3586981cbc9391
NickBwalley <nickbwalley@gmail.com>
2020-07-14 12:42:47 +0300

    cpp

--- DefinePreprocessor_Const/main.cpp
+++ DefinePreprocessor_Const/main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
-
 using namespace std;
+//  Defining constants using the #define identifier value
+#define LENGTH 10
+#define WIDTH 15
+#define NEWLINE '\n'
+
+//function prototypes
+void findArea();
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+        findArea();
 }
+
+//function declaration using the #define identifier value
+void findArea(){
+    int area;
+    area = LENGTH * WIDTH;
+    cout << "Area is: " << area;
+    cout << NEWLINE;
+}
+
+//function declaration using the const keyword
+
