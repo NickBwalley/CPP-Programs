64f7140f7bd4bcb0369fdaef451aeefbc003503f
NickBwalley <nickbwalley@gmail.com>
2020-07-24 11:01:19 +0300

    Cpp

--- Modifier_Types/main.cpp
+++ Modifier_Types/main.cpp
@@ -6,3 +6,7 @@ int main()
     cout << "Hello world!" << endl;
     return 0;
 }
+
+//C++ allows the char, int, and double data types to have modifiers preceding
+//them. A modifier is used to alter the meaning of the base type so that it more
+//precisely fits the needs of various situations.
