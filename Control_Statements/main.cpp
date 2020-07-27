0c1be25dac084a407ceee1c5fb4bc4a27139fd15
NickBwalley <nickbwalley@gmail.com>
2020-07-27 10:14:58 +0300

    cpp

--- Control_Statements/main.cpp
+++ Control_Statements/main.cpp
@@ -1,10 +1,58 @@
 #include <iostream>
-#include "If_Else.h"
-#include "Else_If.h"
 using namespace std;
+
+//function prototypes
+void ifStatement();
+void elseIf();
 
 int main()
 {
-    //  If_Else nick;
-    //  Else_If nick;
+    //ifStatement();
+    elseIf();
+}
+
+//function definitions
+void ifStatement(){
+    int num;
+    cout << "Enter a Number: " << endl;
+    cin >> num;
+    if(num % 2 == 0){
+        cout << num << " is an Even Number..." << endl;
+    }
+    else{
+        cout << num << " is an Odd Number..." << endl;
+    }
+}
+
+void elseIf(){
+    int num;
+    cout<<"Enter a number to check grade:" << endl;
+    cin>>num;
+    if (num <0 || num >100)
+    {
+        cout<<"wrong number" << endl;
+    }
+    else if(num >= 0 && num < 50){
+        cout<<"Fail" << endl;
+    }
+    else if (num >= 50 && num < 60)
+    {
+        cout<<"D Grade" << endl;
+    }
+    else if (num >= 60 && num < 70)
+    {
+        cout<<"C Grade" << endl;
+    }
+    else if (num >= 70 && num < 80)
+    {
+        cout<<"B Grade" << endl;
+    }
+    else if (num >= 80 && num < 90)
+    {
+        cout<<"A Grade" << endl;
+    }
+    else if (num >= 90 && num <= 100)
+    {
+        cout<<"A+ Grade" << endl;
+    }
 }
