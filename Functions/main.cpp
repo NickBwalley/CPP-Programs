b94c47047554760b2f3dd1078f8fafeda91f7902
NickBwalley <nickbwalley@gmail.com>
2020-07-31 22:15:13 +0300

    cpp

--- Functions/main.cpp
+++ Functions/main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
-
 using namespace std;
-
+//function prototype
+void passByValue(int value);
 int main()
 {
-    cout << "Hello world!" << endl;
+    int x, y;
+    x = 39;
+    passByValue(x);
+    cout << "PassByValue outside the function  is: " << x << endl;
+
     return 0;
+}
+
+//pass(call) by value -> Original value of the function is not modified in the function;
+void passByValue(int value){
+    value = 49;
+    cout << "value inside the function is: " << value << endl;
+}
+
+// pass(call) by reference -> Original value is modified because we pass reference(address of the value)
+void passByReference(int value){
+
 }
