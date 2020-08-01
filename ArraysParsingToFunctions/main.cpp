0dc3c87021ad137d181cb6aacddcd3eef9b7615b
NickBwalley <nickbwalley@gmail.com>
2020-08-01 10:42:57 +0300

    cpp

--- ArraysParsingToFunctions/main.cpp
+++ ArraysParsingToFunctions/main.cpp
@@ -4,15 +4,19 @@ using namespace std;
 
 void printNumbers(int theArray[], int sizeOfArray);
 void printArray(int myArray[], int mySize);
+void array2(int biiy[], int sizeOfArray);
 
 int main()
 {
     int nickie[5] = {23,39,33,79,298};
     int chelsea[7] = {29,39,48,71,188,189,169};
+    int biiy[7] = {29,39,48,71,188,189,169};
 
     printNumbers(nickie, 5);
     cout << "****************" << endl;
     printArray(chelsea, 7);
+    cout << "****************" << endl;
+    array2(biiy, 7);
 }
 
 void printNumbers(int theArray[], int sizeOfArray){
@@ -26,5 +30,11 @@ void printNumbers(int theArray[], int sizeOfArray){
 void printArray(int myArray[], int mySize){
     for(int z = 0; z < mySize; z++){
         cout << myArray[z] << endl;
+    }
+}
+
+void array2(int biiy[], int sizeOfArray){
+    for(int j = 0; j < sizeOfArray; j++){
+        cout << biiy[j] << endl;
     }
 }
