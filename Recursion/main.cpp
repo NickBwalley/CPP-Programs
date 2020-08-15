d323465d96637a095a23af98f487dc194cd734bc
NickBwalley <nickbwalley@gmail.com>
2020-08-15 14:40:49 +0300

    cpp

--- Recursion/main.cpp
+++ Recursion/main.cpp
@@ -1,15 +1,28 @@
 #include <iostream>
 #include "NickyFactorial.h"
 using namespace std;
+// function prototype
+int recursion(int x);
 
 int main()
 {
-    NickyFactorial nick;
-    int result, num;
-    num = 5;
-    result = nick.NumberFactorial(num);
-    cout << "The Factorial of " << num << " is " << result << endl;
-    cout << "----------------" << endl;
+    int res;
+    res = recursion(5);
+    cout << res << endl;
+//    NickyFactorial nick;
+//    int result, num;
+//    num = 5;
+//    result = nick.NumberFactorial(num);
+//    cout << "The Factorial of " << num << " is " << result << endl;
+//    cout << "----------------" << endl;
 
+
+}
+
+int recursion(int x){
+    if(x==1)
+        return 1;
+    else
+        return x * recursion(x-1);
 
 }
