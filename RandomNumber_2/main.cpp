95cca3211608222ed54852eb1e8e51665399302b
NickBwalley <nickbwalley@gmail.com>
2020-08-15 14:15:17 +0300

    cpp

--- RandomNumber_2/main.cpp
+++ RandomNumber_2/main.cpp
@@ -2,11 +2,21 @@
 #include "NickFury.h"
 using namespace std;
 
+class RandGenerator{
+public:
+    RandGenerator(){
+        srand(time(0));
+        for(int i = 1; i <= 10; i++){
+            cout << rand()%10;
+        }
+    }
 
+};
 
 int main()
 {
-    NickFury nickie;
-    nickie.randomNumbers();
+
+//    NickFury nickie;
+//    nickie.randomNumbers();
 
 }
