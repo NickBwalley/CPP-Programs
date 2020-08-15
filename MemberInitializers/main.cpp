7987cf914715416cce94a9c10817fdf445d5a2ca
NickBwalley <nickbwalley@gmail.com>
2020-08-15 13:45:29 +0300

    cpp

--- MemberInitializers/main.cpp
+++ MemberInitializers/main.cpp
@@ -2,8 +2,21 @@
 #include "nicky.h"
 using namespace std;
 
+class FormulaOne{
+private:
+    int red_bull;
+    const int mercedes_benz;
+public:
+    FormulaOne(int a, int b): red_bull(a), mercedes_benz(b){
+        cout << "red_bull: " << a << " KM/H" << endl;
+        cout << "mercedes_benz: " << b << " KM/H"<< endl;
+    }
+};
+
 int main()
 {
-    nicky chelsea(79,99);
-    chelsea.print();
+    FormulaOne driver(300, 330);
+//    nicky chelsea(79,99);
+//    chelsea.print();
+
 }
