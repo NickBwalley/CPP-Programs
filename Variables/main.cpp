7432bfeeebceb9fab39875c6247e6cdf65075c72
NickBwalley <nickbwalley@gmail.com>
2020-08-19 11:22:09 +0300

    cpp

--- Variables/main.cpp
+++ Variables/main.cpp
@@ -4,9 +4,12 @@ using namespace std;
 
 //  global variable -> Global variables are automatically initialized by the system as compared to
 //  local variables -> Local variables are not automatically initialized by the system as you must initialize them
-    int  g;
+    int z = 50;
+
 int main()
 {
-    int g = 40;
+    int g;
+    int z = 49;
     cout << g << endl;
+    cout << "Global-Variable: " << ::z << endl;
 }
