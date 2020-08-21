9b78754fa1ea68216e7c0279e3b1bccd5ea8ad6a
NickBwalley <nickbwalley@gmail.com>
2020-08-21 10:44:13 +0300

    cpp

--- DBT_Quiz1/main.cpp
+++ DBT_Quiz1/main.cpp
@@ -2,17 +2,41 @@
 #include <cstdlib>
 #include <ctime>
 using namespace std;
+
 // function prototype
 int randomNumberGenerator();
+bool checkRandomNumber();
 
 int main()
 {
-    cout << randomNumberGenerator() << endl;
+    cout << "Random Number: " << randomNumberGenerator() << endl;
+    cout << "---------------------" << endl;
+    cout << checkRandomNumber() << endl;
 }
 
+//function generate random Number
 int randomNumberGenerator(){
-    int rand;
+    int randomNumber;
     srand(time(0));
-    rand = 1+rand()%300;
-    return rand;
+    randomNumber = 1+rand()%300;
+    return randomNumber;
+}
+//function check if random Number is Even or odd
+bool checkRandomNumber(){
+    int flag = 0;
+    for(int i = 2; i < randomNumberGenerator()/2; i++){
+        if(randomNumberGenerator()%i == 0){
+            printf("%d is not a Prime Number", randomNumberGenerator());
+            cout << endl;
+            return false;
+            break;
+        }
+
+    }
+    if (flag == 0){
+        printf("%d is a Prime Number", randomNumberGenerator());
+        cout << endl;
+        return true;
+    }
+
 }
