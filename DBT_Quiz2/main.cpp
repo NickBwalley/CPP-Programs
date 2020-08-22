7f34af3c6aee3530688670307a783e1beb3125bb
NickBwalley <nickbwalley@gmail.com>
2020-08-22 22:03:51 +0300

    cpp

--- DBT_Quiz2/main.cpp
+++ DBT_Quiz2/main.cpp
@@ -4,6 +4,16 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int x, y, p, q;
+    p = 7;
+    q = p;
+    x = ++p;
+    y = q++;
+
+    cout <<"Value P: " << p << endl;
+    cout <<"Value q: "<< q << endl;
+    cout <<"Value x: "<< x << endl;
+    cout <<"Value y: "<< y << endl;
+
     return 0;
 }
