e4dfe3ac23c844adf689cd7f7fc975b9b2bffe5d
NickBwalley <nickbwalley@gmail.com>
2020-08-23 15:36:04 +0300

    cpp

---
+++ CppConcepts/Cpp_Comments/Comments/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "CPP COMMENTS!.." << endl;
+    // This is a single line comment
+    /* This is a multi line comment */
+    return 0;
+}
