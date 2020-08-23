ae9e255246e19c74d49e1ddbc599d110cc7078ae
NickBwalley <nickbwalley@gmail.com>
2020-08-23 15:33:09 +0300

    cpp

--- CppConcepts/Cpp_Operators/Operators/main.cpp
+++ CppConcepts/Cpp_Operators/Operators/main.cpp
@@ -7,3 +7,17 @@ int main()
     cout << "Hello world!" << endl;
     return 0;
 }
+/*OPERATORS
+1. Arithmetic Operators -> (+, -, *, /, %, ++, -- )
+2. Assignment Operators -> (=, +=, -=, *=, /=, %= )
+3. Relational Operators -> (==, !=, >, >=, <, <= )
+4. Logical Operators -> (&&, ||) &&-> return true if all are true, ||-> return true if one condition is fulfilled
+5. Bitwise Operators ( can be used to perform operations on individual bits, used alongside char and int datatypes)
+    & -> Binary AND
+    | -> Binary OR
+    ^ -> Binary XOR
+    ~ -> Binary Ones complement
+    <<  Binary Shift Left
+    >> Binary Shift Right
+
+*/
