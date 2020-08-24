b63b98fc10f111cf0dccf5e0a49a4ae2499af976
NickBwalley <nickbwalley@gmail.com>
2020-08-24 11:19:17 +0300

    cpp

--- CppConcepts/Cpp_Comments/DBT_Quiz5/main.cpp
+++ CppConcepts/Cpp_Comments/DBT_Quiz5/main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
 
 using namespace std;
+//question 4(a)(i)
+double hptTri(double s1, double s2)
+{
+    float res1 = (double)s1;
+    float res2 = (double)s2;
+    float results = res1 + res2;
+    return results;
 
+}
+//question 4(a)(ii)
+int smallNum(int a, int b, int c)
+{
+    return a;
+}
+//question 4(a)(iii)
+void myName()
+{
+    cout << "This is a function that returns null value and expects null arguments " << endl;
+}
+//question 4(a)(iv)
+double myConverter(int a)
+{
+    return a;
+}
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout << hptTri(3.4, 9.44) << endl;
+    cout << smallNum(4, 5, 6) << endl; // returns 4
+    myName();
+    cout << myConverter(44) << endl;
+
     return 0;
 }
