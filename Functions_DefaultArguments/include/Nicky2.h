99514316bc2e1708534e2598fc92f0ce9ba1ddb5
NickBwalley <nickbwalley@gmail.com>
2020-09-06 12:36:33 +0300

    cpp

---
+++ Functions_DefaultArguments/include/Nicky2.h
@@ -0,0 +1,15 @@
+#ifndef NICKY2_H
+#define NICKY2_H
+
+
+class Nicky2
+{
+    public:
+        Nicky2();
+
+    protected:
+
+    private:
+};
+
+#endif // NICKY2_H
---
+++ Functions_DefaultArguments/src/Nicky2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Nicky2.h"
+using namespace std;
+
+Nicky2::Nicky2()
+{
+
+}
